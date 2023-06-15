de01215d8573c4d839a7439a93c03fc30c3ea6b4
Thibault <thib.panis@gmail.com>
2023-06-15 16:51:04 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
 # TableMerger
+
+## WHAT IT DOES :
+    - This script takes 2 csv tables : a base table and an input table.
+    - It takes a selection of columns (fields) from the input table, and appends their data to the base table.
+        (So at his core, this script just build a simple SQL "LEFT JOIN")
+    - It returns the result in an output csv, without altering the two original files.
+    - Only rows from the input table that match a certain amount of configurables conditions are appended to the base table :
+        remanining, non-matching data is not kept.
+    - All rows from the base table are preserved : they are just completed with the right data from the input table.
+    - If one or more rows from the input data match the same row from the base data, the script just keeps the first and ditches the others.
+        (The number of such deleted "ambivalents rows" is logged.)
+
+## TECHNICAL SETUP : 
+    -You must have python 3 installed on your machine, or in your python env if you use one (> 3.10 is preferably)
+    -You also need to install the package pandas, by typing "pip install pandas" into your machine prompt. ("cmd" terminal on Windows)
+    -The previous instruction will only work if you have not installed pip yet.
+        If it's not the case : install it. (Check these instructions on windows https://phoenixnap.com/kb/install-pip-windows )
+
+
+## INSTRUCTIONS :
+    -This script must be executed within the same directory as the base and input data files
+    -The output data file will be writen within the same directory
+    -CSV files must not be opened in other softwares (excel, for example)
+    -At launch, a "menu" (dirty code)
+
+
+## CONFIGURATION :
+    Modify theses constants for customization.
+    
+    - BASE_FILENAME (str) : The name of the base file. CSV format is required here, with ";" separators.
+    - INPUT_FILENAME (str) : The name of the input file. CSV format is required here, with ";" separators 
+    - OUTPUT_FILENAME (str) : The name you want for your output table.
+    - ALL_FIELDS_SELECTED_BY_DEFAULT (bool) : When set to True, all fields from the input base are by default selected into the menu. (you can still unselect them into the menu)
+    
+    
+    - DEPTH_WITHIN_RANGE_CONFIG (dict) : 
+        A special configuration dict that defines how base table and input table are joined based on fields that are closed but not stricly equal.
+        (Typycally the Depth)
+        In that case, the match is validated if the input Depth value is close to the base Depth value, "close enough" being defined by a range.
+        
+        The rules below specify how this range is determined.
+            - "defaultMarginValue" is the base acceptance range.
+            - When "alwaysUseDefaultValue" is True, the script always applies defaultMarginValue and ignores the "marginErrorVariationsConfig"
+            - When "alwaysUseDefaultValue" is False, the script consults the "marginErrorVariationsConfig" : the accepted marge then depends on the base table Depth value.
+        
+        Example (do not modify please):
+        {
+            "defaultMarginValue": 0.8,
+            "alwaysUseDefaultValue": True,
+            "marginErrorVariationsConfig": [
+                {"min": 0, "max": 200, "margin +/-": 0.8},
+                {"min": 200, "max": 800, "margin +/-": 0.8},
+                {"min": 800, "max": 2000, "margin +/-": 0.8},
+                {"min": 2000, "max": 1000000, "margin +/-": 0.8},
+            ],
+        }
+    
+    - JOIN_FIELDS_CONFIGURATION (list[dict]) : 
+        The main configuration object that defines how base table and input table are joined. (on which fields and on at which conditions)
+        Each element of this list represents a relation that must be respected between two rows of the table for being merged together.
+        Each relation is caracterized by two fields involved (one on base table, and another on input table), and by a join condition.
+        For each item of the configuration list : 
+            - FieldNameInBase and FieldNameInInput are required. They define respectively the field in base table and the field in input table that must match.
+            - JoinCondition is required too. It can only accepts two value (for now) : "Equality" or "WithinRange"
+            - If JoinCondition is "WithinRange", a "SpecialConditionConfig" field is required too, and must follow a format such as "DEPTH_WITHIN_RANGE_CONFIG" described above.
+        
+        Example (do not modify please):
+        [
+            {
+                "FieldNameInBase": "Station",
+                "FieldNameInInput": "Station",
+                "JoinCondition": "Equality",
+            },
+            {
+                "FieldNameInBase": "Depth [m]",
+                "FieldNameInInput": "Depth [m]",
+                "JoinCondition": "WithinRange",
+                "SpecialConditionConfig": DEFAULT_DEPTH_WITHIN_RANGE_CONFIG,
+            },
+        ]
---
+++ table_merger.py
@@ -0,0 +1,317 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 12 12:08:29 2022
+
+@author: Thibault P
+"""
+
+import pandas as pd
+import time
+import sqlite3
+
+
+'''
+README : https://github.com/PhillipsHoward/TableMerger
+
+'''
+
+
+
+BASE_FILENAME = "SWINGS_CTD_clean_nut.csv"
+INPUT_FILENAME = "swings_ctd_completed_O2.csv"
+OUTPUT_FILENAME = "output.txt"
+ALL_FIELDS_SELECTED_BY_DEFAULT = False
+DEPTH_WITHIN_RANGE_CONFIG = {
+    "defaultMarginValue": 0.8,
+    "alwaysUseDefaultValue": True,
+    "marginErrorVariationsConfig": [
+        {"min": 0, "max": 200, "margin +/-": 0.8},
+        {"min": 200, "max": 800, "margin +/-": 0.8},
+        {"min": 800, "max": 2000, "margin +/-": 0.8},
+        {"min": 2000, "max": 1000000, "margin +/-": 0.8},
+    ],
+}
+JOIN_FIELDS_CONFIGURATION = [
+    {
+        "FieldNameInBase": "Station",
+        "FieldNameInInput": "Station",
+        "JoinCondition": "Equality",
+    },
+    {
+        "FieldNameInBase": "Depth [m]",
+        "FieldNameInInput": "Depth [m]",
+        "JoinCondition": "WithinRange",
+        "SpecialConditionConfig": DEPTH_WITHIN_RANGE_CONFIG,
+    },
+]
+
+
+def extract_potentials_fields_to_add_from_input(input_data):
+    """Extract from input_data the potential fields to append to base. Format it into a clean dict"""
+    return input_data.columns.values.tolist().copy()[  # TODO This should not stay hardcoded like that
+        1:
+    ]
+
+
+def generate_selection_menu(all_fields, all_selected=False):
+    """
+    Let the user chose the fields to append into base_data
+    Sorry for this dirty function. This should be refactored someday.
+    """
+    print("Select the fields to append from the new input matrix to the reference one.")
+
+    if all_selected:
+        selected_fields = all_fields.copy()
+    else:
+        selected_fields = []
+    exit_index = len(all_fields) + 1
+
+    def print_menu():
+        """Generate dynamically the menu options selection"""
+        for index, value in enumerate(all_fields):
+            print(
+                str(index + 1),
+                "--",
+                value,
+                "SELECTED" if value in selected_fields else "UNSELECTED",
+            )
+        print("----")
+        print(str(exit_index), "--", "VALIDATE")
+
+    def reorder_fields(selected_fields, all_fields):
+        """Reorder selected fields into the original matrix order"""
+        ordered_fields = []
+        for field in all_fields:
+            if field in selected_fields:
+                ordered_fields.append(field)
+        return ordered_fields
+
+    while True:
+        print_menu()
+        option = ""
+        try:
+            option = int(input("Type the field number to select it or unselect it : "))
+        except:
+            print("Wrong input. Please enter a number ...")
+        # Check what choice was entered and act accordingly
+        if 1 <= option and option <= len(all_fields):
+            if all_fields[option - 1] in selected_fields:
+                print("Field unselected")
+                selected_fields.remove(all_fields[option - 1])
+            else:
+                print("Field selected")
+                selected_fields.append(all_fields[option - 1])
+        elif option == exit_index:
+            print("Selection done.")
+            return reorder_fields(selected_fields, all_fields)
+            break
+        else:
+            print(
+                "Invalid option. Please enter a number between 1 and "
+                + str(exit_index)
+                + "."
+            )
+
+
+class DataTablesMerger:
+    def __init__(
+        self,
+        base_data,
+        input_data,
+        fields_to_append_list,
+        join_fields_configuration,
+    ):
+        self.base_data = base_data
+        self.input_data = input_data
+        self.fields_to_append_list = fields_to_append_list
+        self.join_fields_configuration = join_fields_configuration
+        self.pre_query_select_parts = []
+        self.pre_query_join_parts = []
+        self.join_fields_name_list = [
+            configuration_element["FieldNameInBase"]
+            for configuration_element in self.join_fields_configuration
+        ]
+        self.start_time = time.time()
+
+    def logger(self, raw_message="", display_time=True):
+        message = ""
+        if display_time:
+            message = f"-- time : {(time.time() - self.start_time):.2f} s - "
+        else:
+            message += "-- "
+        print(message + raw_message)
+
+    def get_marge_error_according_to_config(self, value, join_within_range_config):
+        variations_config = join_within_range_config["marginErrorVariationsConfig"]
+        default_margin_error = join_within_range_config["defaultMarginValue"]
+
+        for variation_range in variations_config:
+            if variation_range["min"] < value and variation_range["max"] >= value:
+                return variation_range["margin +/-"]
+        return default_margin_error
+
+    def add_helpers_columns_for_join_within_range_field(
+        self, field_in_input, join_within_range_config
+    ):
+        marg_of_error_column_name = f"{field_in_input}_mg_error"
+        max_limit_column_name = f"max_{field_in_input}"
+        min_limit_column_name = f"min_{field_in_input}"
+
+        if join_within_range_config["alwaysUseDefaultValue"] is True:
+            self.input_data[marg_of_error_column_name] = join_within_range_config[
+                "defaultMarginValue"
+            ]
+        else:
+            self.input_data[marg_of_error_column_name] = self.input_data.apply(
+                lambda row: self.get_marge_error_according_to_config(
+                    row[field_in_input],
+                    join_within_range_config=join_within_range_config,
+                ),
+                axis=1,
+            )
+        self.input_data[min_limit_column_name] = self.input_data.apply(
+            lambda row: row[field_in_input] - row[marg_of_error_column_name],
+            axis=1,
+        )
+        self.input_data[max_limit_column_name] = self.input_data.apply(
+            lambda row: row[field_in_input] + row[marg_of_error_column_name],
+            axis=1,
+        )
+
+    def write_within_range_conditions(self, field_in_base, field_in_input):
+        return [
+            f"base_data.'{field_in_base}' > input_data.'min_{field_in_input}'",
+            f"base_data.'{field_in_base}' <= input_data.'max_{field_in_input}'",
+        ]
+
+    def setup_one_within_range_join(
+        self, field_in_base, field_in_input, join_within_range_config
+    ):
+
+        self.add_helpers_columns_for_join_within_range_field(
+            field_in_input=field_in_input,
+            join_within_range_config=join_within_range_config,
+        )
+        query_conditions = self.write_within_range_conditions(
+            field_in_base, field_in_input
+        )
+        self.pre_query_join_parts += query_conditions
+
+    def setup_within_range_joins(self):
+        for join_field_configuration in self.join_fields_configuration:
+            if (
+                "JoinCondition" in join_field_configuration
+                and join_field_configuration["JoinCondition"] == "WithinRange"
+            ):
+                self.setup_one_within_range_join(
+                    field_in_base=join_field_configuration["FieldNameInBase"],
+                    field_in_input=join_field_configuration["FieldNameInInput"],
+                    join_within_range_config=join_field_configuration[
+                        "SpecialConditionConfig"
+                    ],
+                )
+
+    def setup_equality_joins(self):
+        for join_field_configuration in self.join_fields_configuration:
+            if (
+                "JoinCondition" in join_field_configuration
+                and join_field_configuration["JoinCondition"] == "Equality"
+            ):
+                self.pre_query_join_parts += [
+                    f"base_data.'{join_field_configuration['FieldNameInBase']}' = input_data.'{join_field_configuration['FieldNameInInput']}'"
+                ]
+
+    def build_final_query(self):
+        select_query_section = ", ".join(
+            ["SELECT base_data.*"]
+            + [
+                f"input_data.'{field}'"
+                if field not in self.join_fields_name_list
+                else f"input_data.'{field}' AS '{field}_input'"
+                for field in self.fields_to_append_list
+            ]
+        )  # We add suffixe "_input" only for fields that are used as join fields,
+        # because otherwise they would lead to falsy ambivalent row deletions. (see "drop_ambivalent_matching_rows()")
+        join_query_section = " AND ".join(self.pre_query_join_parts)
+
+        query = f"""
+            {select_query_section}
+            FROM base_data
+            LEFT JOIN input_data
+            ON {join_query_section} 
+            """
+        return query
+
+    def join_tables(self, join_query):
+        conn = sqlite3.connect("TheBlattabase")
+        self.base_data.to_sql("base_data", conn, index=False, if_exists="replace")
+        self.input_data.to_sql("input_data", conn, index=False, if_exists="replace")
+        raw_data = pd.read_sql_query(join_query, conn)
+        return raw_data
+
+    def drop_ambivalent_matching_rows(self, data):
+
+        initial_rows_number = len(data.index)
+        cleaned_data = data.drop_duplicates(
+            subset=self.join_fields_name_list, keep="first"
+        )
+        self.logger(
+            f"{initial_rows_number-len(cleaned_data.index)} ambivalent rows had to be removed."
+        )
+        return cleaned_data
+
+    def launch(self):
+        self.start_time = time.time()
+        self.logger("Job launched")
+        self.logger("-------------", display_time=False)
+
+        self.logger("Prepare join.")
+        self.setup_equality_joins()
+        self.setup_within_range_joins()
+        self.logger("-------------", display_time=False)
+
+        self.logger("Build query")
+        join_query = self.build_final_query()
+        # print(join_query) # kept for debug purpose
+        self.logger("-------------", display_time=False)
+
+        self.logger(
+            "Merge the two tables according to configured matching conditions. This could take a while."
+        )
+        joined_data = self.join_tables(join_query)
+        self.logger("-------------", display_time=False)
+
+        self.logger("Removing duplicated rows.")
+        final_data = self.drop_ambivalent_matching_rows(joined_data)
+        self.logger("-------------", display_time=False)
+
+        self.logger("Job achieved.")
+        self.logger("-------------", display_time=False)
+
+        return final_data
+
+
+def main():
+    base_data = pd.read_csv(
+        BASE_FILENAME, sep=";", encoding="ISO-8859-1", engine="python"
+    )
+    input_data = pd.read_csv(
+        INPUT_FILENAME, sep=";", encoding="ISO-8859-1", engine="python"
+    )
+    potentials_fields_to_add = extract_potentials_fields_to_add_from_input(input_data)
+    selected_fields = generate_selection_menu(
+        potentials_fields_to_add, ALL_FIELDS_SELECTED_BY_DEFAULT
+    )
+
+    tables_merger = DataTablesMerger(
+        base_data=base_data,
+        input_data=input_data,
+        fields_to_append_list=selected_fields,
+        join_fields_configuration=JOIN_FIELDS_CONFIGURATION,
+    )
+    output = tables_merger.launch()
+    output.to_csv(OUTPUT_FILENAME, sep="\t", index=False)
+
+
+main()
+
